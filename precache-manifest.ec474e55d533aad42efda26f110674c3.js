81f228bd990a7dec0f0a6c1c0ad3229b92005181
notoriousarun <notoriousarun@protonmail.com>
2020-09-17 10:16:58 +0530

    Updates

---
+++ precache-manifest.ec474e55d533aad42efda26f110674c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef8a4e2ccc2b653281850cd96715b96",
+    "url": "/xWatch/index.html"
+  },
+  {
+    "revision": "83c38d5b4ae20f5251db",
+    "url": "/xWatch/static/css/main.5991f196.chunk.css"
+  },
+  {
+    "revision": "dfb80aedc7c41f37ecc7",
+    "url": "/xWatch/static/js/2.62f3d26b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/xWatch/static/js/2.62f3d26b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c38d5b4ae20f5251db",
+    "url": "/xWatch/static/js/main.74a80014.chunk.js"
+  },
+  {
+    "revision": "0596f136ad3e9610306e",
+    "url": "/xWatch/static/js/runtime-main.970ea15a.js"
+  }
+]);
